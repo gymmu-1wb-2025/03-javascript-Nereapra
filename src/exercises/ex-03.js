fe459ea3f511d942baf7dedce69a13a45597013b
Nerea Prado <e273623@edu.sbl.ch>
2026-01-23 09:30:22 +0100

    Aufgabe 3 gemacht

---
+++ src/exercises/ex-03.js
@@ -0,0 +1,7 @@
+let name= process.argv[2]
+let age= Number(process.argv[3])
+let ageMyMatur = age + 4;
+
+console.log (`Hallo ${name}! `);
+console.log (`Du bist aktuell ${age} Jahre alt`);
+console.log (`An der Matur wirst du ungefähr ${ageMyMatur} Jahre alt sein.`);
